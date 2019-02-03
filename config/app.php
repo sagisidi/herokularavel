7c7d419c549db8d62158ed9ee8a3cf38f078dd5d
Sagi Sidi <sagisidi@gmail.com>
2019-02-03 20:24:08 +0200

    s

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://peaceful-wave-91890.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
